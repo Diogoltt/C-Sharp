df7112c7cee765d801cd3bfae5aef23e0853869c
Diogoltt <diogoleite09@gmail.com>
2024-10-23 16:08:46 -0400

    new: 3 Exercícios de fixação, registros do dia 23/10/2024

---
+++ Aula1/Exemplo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Exemplo {
+    internal class Program {
+        static void Main(String[] x) {
+            int idade = 25;
+            double saldo = 10.50;
+            string nome = "Maria Conceição";
+
+            //Console.WriteLine("Olá " + nome + ", você tem " + idade + " anos de idade e um saldo de R$" + saldo);
+            
+            //Console.WriteLine("{0} tem {1} anos de idade e tem um saldo igual a {2:f2} reais", nome, idade, saldo);
+            
+            Console.WriteLine($"{nome} tem {idade} anos de idade e tem um saldo igual a {saldo:f2} reais");
+        }
+    }
+}
---
+++ Aula1/Exemplo2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Exemplo {
+    internal class Program {
+        static void Main() {
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            String nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f; 
+
+            Console.WriteLine($"Olá, {obj1}, você é do genero {genero} e tem {n1} anos");
+    }
+}
+}
