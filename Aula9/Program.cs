eaf28244b41f67f27f9a9e44848c2b80193ccaa3
Diogoltt <diogoleite09@gmail.com>
2024-11-08 16:59:50 -0400

    new: Aula9 e desafio em grupo

---
+++ Aula9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Aula9 {
+    class Program {
+        static int ParOuImpar(int num) {
+            if (num % 2 == 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        static string ParOuImpar(double num) {
+            if (num % 2 == 0) {
+                return "Par";
+            } else {
+                return "Impar";
+            }
+        }
+
+        static void Pi() {
+            System.Console.WriteLine("3.14");
+        }
+
+        static void teste() {
+            
+            /*
+            System.Console.WriteLine("Digite um número: ");
+            double num = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Número é: " + ParOuImpar((int)num));
+            */
+
+            // Pi();
+
+        }
+    }
+}
