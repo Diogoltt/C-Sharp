d30b9b14c2085aa2a078af3f2f8f0c4f9b404c45
Diogoltt <diogoleite09@gmail.com>
2024-11-11 15:29:51 -0400

    Aula10: ref/out

--- Aula10/VariaveisImplicitas.cs
+++ Aula10/VariaveisImplicitas.cs
@@ -1,7 +1,7 @@
 using System;
 namespace Aula10 {
     public class VariaveisImplicitas {
-        static void Main(string[] args) {
+        static void teste() {
             var nome = "C#";
             System.Console.WriteLine(nome);
             System.Console.WriteLine(nome.GetType());
@@ -13,6 +13,30 @@ namespace Aula10 {
             var altura = 1.60;
             System.Console.WriteLine(altura);
             System.Console.WriteLine(altura.GetType());
+
+            const double pi = 3.145;
+            System.Console.WriteLine(pi);
+            System.Console.WriteLine(pi.GetType());
+
+            var lista = new List<int> {1, 2, 3, 4, 5};
+
+            foreach (var item in lista) 
+            {
+                System.Console.WriteLine(item);
+            }
+
+            var dicionario = new Dictionary<string, int>
+            {
+                {"C#", 1},
+                {"Java", 2},
+                {"Python", 3}
+            };
+            System.Console.WriteLine(dicionario.GetType());
+
+            foreach(var item in dicionario)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ Aula10/reff.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Aula10 {
+    class reff
+    {
+        static int Quadrado(int x)
+        {
+            return x * x;
+        }    
+        
+        static int QuadradoRef(ref int x)
+        {
+            return x = x * x;
+        }
+        
+        static int QuadradoOut(out int x)
+        {
+            x = 6;
+            return x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(Quadrado(a)); // Imprime o quadrado de 'a' mas não altera 'a'
+            Console.WriteLine(a); // Imprime o valor original de 'a' que é 10
+
+            QuadradoRef(ref a); // Altera o valor de 'a' para o seu quadrado
+            Console.WriteLine(a); // Agora 'a' será 100
+
+            int b;
+            int resultadoOut = QuadradoOut(out b); // Define 'b' como 6 e calcula seu quadrado
+            Console.WriteLine(b); // Imprime 6, que é o valor atribuído dentro de QuadradoOut
+            Console.WriteLine(resultadoOut); // Imprime 36, que é o quadrado de 6
+        }
+    }
+}
